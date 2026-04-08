fbe250505356af8f982dd7cb77c4fafc8c159cad
PBrinthan <bpak0001@student.monash.edu>
2026-04-08 15:21:46 +1000

    Use float for num1's conversion also

--- calc.py
+++ calc.py
@@ -6,7 +6,7 @@ def compute(expression):
         '1 - 2' -> -1
     """
     num0, operator, num1 = expression.split(' ')
-    num0, num1 = float(num0), int(num1)
+    num0, num1 = float(num0), float(num1)
 
     if operator == '+':
         return num0 + num1
